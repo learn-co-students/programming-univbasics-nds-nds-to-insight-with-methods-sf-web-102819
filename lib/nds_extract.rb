138dc2224e488481c1d6ef60474289f1dee3ba33
Joseph  Louie <joseph161216@gmail.com>
2019-10-23 04:21:16 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,17 +6,13 @@ require 'directors_database'
 # { directorOne => allTheMoneyTheyMade, ... }
 
 def directors_totals(nds)
-  result = {
-    "Stephen Spielberg"=>1357566430,
-    "Russo Brothers"=>2281002470,
-    "James Cameron"=>2571020373,
-    "Spike Lee"=>256624217,
-    "Wachowski Siblings"=>806180282,
-    "Robert Zemeckis"=>1273838385,
-    "Quentin Tarantino"=>662738268,
-    "Martin Scorsese"=>636812242,
-    "Francis Ford Coppola"=>509719258
-  }
+  result = {}
+  i = 0 
+  while i < nds.length do 
+    director = nds[i]
+    result[director[:name]] = gross_for_director(director)
+    i += 1
+  end
   result
 end
 
